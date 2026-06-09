e3dd0419921ef36342ea4cb6663dad6c61595be4
NotArt <noterisarthur42@gmail.com>
2026-06-09 15:04:54 +0200

    Parse torrent

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,9 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import parseTorrent from "parse-torrent";
+import fs from "fs";
+import path from "path";
 
 dotenv.config();
 
@@ -23,3 +26,19 @@ mongoose
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server running");
 });
+
+
+try {
+  const torrentPath = path.join(__dirname, "big-buck-bunny.torrent");
+  const torrent = fs.readFileSync(torrentPath);
+  console.log(torrent);
+
+  const parsed = parseTorrent(torrent);
+  console.log("Torrent Name:", parsed.name);
+  console.log("Info Hash:", parsed.infoHash);
+  if ('files' in parsed) {
+    console.log("Files:", parsed.files?.map((f: any) => f.name));
+  }
+} catch (err) {
+  console.error("Failed to read/parse torrent:", err);
+}
